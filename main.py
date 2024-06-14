0055760cc90b4acdf54a33dec8bfc91edf94794b
Dawid Ziobrowski (260284) <260284@student.pwr.edu.pl>
2024-06-14 20:02:51 +0200

    Add db_name to env

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-CONNECTION_STRING = "mssql+pyodbc://@DESKTOP-6NDP85E/hurtownie_projekt?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+import os
+CONNECTION_STRING = f"mssql+pyodbc://@{os.environ["DB_NAME"]}/hurtownie_projekt?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
 
 from sqlalchemy import create_engine
 
